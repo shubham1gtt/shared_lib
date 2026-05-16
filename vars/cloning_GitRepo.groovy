e2a19f2bd21ee7aabf5e93576cf7d03154366737
shubham1gtt <sgg155655@gmail.com>
2026-05-16 22:04:12 +0530

    Delete vars directory

--- vars/cloning_GitRepo.groovy
+++
@@ -1,4 +0,0 @@
-def call(String gitUrl, String gitBranch){
-    echo "cloning repo from ${gitUrl} in branch ${gitBranch}"
-    git url: "${gitUrl}", branch: "${gitBranch}"
-}
--- vars/docker-cleanup.groovy
+++
@@ -1,3 +0,0 @@
-def call(String dockerHubUser, String projectName, String imageTag){
- sh "docker rmi  ${dockerHubUser}/${projectName}:${imageTag}"
-}
