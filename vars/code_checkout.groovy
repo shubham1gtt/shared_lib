a66e3a3a4c4c76dd9ba49db70031be23bcf367dc
shubham1gtt <sgg155655@gmail.com>
2026-05-16 22:52:59 +0530

    Update code_checkout.groovy

--- vars/code_checkout.groovy
+++ vars/code_checkout.groovy
@@ -2,7 +2,7 @@ def call(string GitUrl, string GitBranch){
   git url: "$(GitUrl)","$(GitBranch)" 
 }
 
-or i can write this as well if requirement is like , will not  change the git url and branch and also dont want to show this on jenkins pipeline
+or i can write this as well if requirement, will not change the git url and branch and also dont want to show this on jenkins pipeline
 def call(){
 
  git url: "https://github.com/LondheShubham153/Wanderlust-Mega-Project.git", branch: "main"
